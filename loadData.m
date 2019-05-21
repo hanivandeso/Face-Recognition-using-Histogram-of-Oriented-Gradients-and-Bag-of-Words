4e835a8000b90e46b4969322d8bc015604916282
hanivandeso <hanivandeso@gmail.com>
2019-05-22 00:24:05 +0700

    feature extraction

--- loadData.m
+++ loadData.m
@@ -12,12 +12,52 @@ listSubFolder = dir('Data\Cropped\**\*.png');
 for i = 1:size(listSubFolder,1)
     imageData{i} = imread(fullfile(listSubFolder(i).folder, listSubFolder(i).name));
     name = strsplit(listSubFolder(i).name, '_');
-    if (isequal(name{1}, 'Ade'))
-        % assign label 0 for Ade
-        label(i,:) = 0;
-    elseif (isequal(name{1}, 'Alvin'))
-        % assign label 1 for Alvin
-        label(i,:) = 1;
+%     if (isequal(name{1}, 'Ade'))
+%         assign label 0 for Ade
+%         label(i,:) = 0;
+%     elseif (isequal(name{1}, 'Alvin'))
+%         assign label 1 for Alvin
+%         label(i,:) = 1;
+%     end
+    switch name{1}
+        case 'Ade'
+            label(i,:) = 0;
+        case 'Alvin'
+            label(i,:) = 1;
+        case 'Ardy'
+            label(i,:) = 2;
+        case 'Daffa'
+            label(i,:) = 3;
+        case 'Dio'
+            label(i,:) = 4;
+        case 'Fadil'
+            label(i,:) = 5;
+        case 'Hanifan'
+            label(i,:) = 6;
+        case 'Liu'
+            label(i,:) = 7;
+        case 'Maula'
+            label(i,:) = 8;
+        case 'Mentari'
+            label(i,:) = 9;
+        case 'Muadz'
+            label(i,:) = 9;
+        case 'Nada'
+            label(i,:) = 10;
+        case 'Redy'
+            label(i,:) = 11;
+        case 'Rezky'
+            label(i,:) = 12;
+        case 'Ria'
+            label(i,:) = 13;
+        case 'Sena'
+            label(i,:) = 14;
+        case 'Wiranata'
+            label(i,:) = 15;
+        case 'Yuda'
+            label(i,:) = 16;
+        case 'Yusuf'
+            label(i,:) = 17;
     end
     disp([listSubFolder(i).name, ' loaded']);
 end
